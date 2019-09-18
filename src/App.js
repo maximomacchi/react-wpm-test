fdb5a67b12037bdc274aa0ce9d4cd6f1e1e78560
Max <maximomacchi@gmail.com>
2019-09-18 15:57:37 -0700

    Timer functionality implemented in App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./styles/App.css";
+
+import Timer from "./components/Timer";
+import StartButton from "./components/Start-Button";
+import { setServers } from "dns";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [time, setTime] = useState(0);
+  const [wordsTyped, setWordsTyped] = useState(0);
+
+  function startTest() {
+    setTime(60);
+    setWordsTyped(0);
+  }
+
+  useEffect(() => {
+    let timeInterval = null;
+    if (time > 0) {
+      timeInterval = setInterval(() => {
+        setTime(time => time - 1);
+      }, 1000);
+    } else {
+      clearInterval(timeInterval);
+    }
+    return () => clearInterval(timeInterval);
+  }, [time]);
+
+  return (
+    <div className="App">
+      <Timer time={time} />
+      <StartButton startTest={startTest} />
+    </div>
+  );
 }
 
 export default App;
