2a85ec83eb446d046d2add418597679b4b70bd5e
Max <maximomacchi@gmail.com>
2019-09-18 16:07:09 -0700

    Form component tracks if word is typed correctly for words passed through props

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Form() {
-  return <div>Form</div>;
+function Form(props) {
+  const [input, setInput] = useState("");
+
+  function updateInput(e) {
+    setInput(e.target.value);
+  }
+
+  useEffect(() => {
+    if (input === props.wordToType) {
+      console.log("Word typed!");
+      setInput("");
+    }
+  }, [input]);
+
+  return (
+    <div>
+      <h3>{props.wordToType}</h3>
+      <form>
+        <input type="text" value={input} onChange={updateInput}></input>
+      </form>
+    </div>
+  );
 }
+
+export default Form;
