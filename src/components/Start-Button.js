23ae73e0c310ab6f0b1ac094aa6315f14634239e
Max <maximomacchi@gmail.com>
2019-09-20 18:21:20 -0700

    Start-Button only renders when test isn't active

--- src/components/Start-Button.js
+++ src/components/Start-Button.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function StartButton({ startTest }) {
-  return (
-    <div>
-      <button onClick={startTest}>Start Test</button>
-    </div>
-  );
+function StartButton({ startTest, testActive }) {
+  if (!testActive) {
+    return (
+      <div>
+        <button onClick={startTest}>Start Test</button>
+      </div>
+    );
+  }
+  return null;
 }
 
 export default StartButton;
